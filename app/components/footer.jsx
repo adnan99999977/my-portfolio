98eb4b0ae039be7c8bbe88db5a7db77b85a954a4
Adnan <naifadnan999@gmmail.com>
2025-12-05 00:55:17 +0600

    add navbar and footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,6 +1,5 @@
 // @flow strict
 import Link from 'next/link';
-import { CgGitFork } from "react-icons/cg";
 import { IoStar } from "react-icons/io5";
 
 function Footer() {
@@ -12,25 +11,18 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Abu Said</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/naif-adnan-1baa5a333/" className="text-[#16f2b3]">NAIF ADNAN</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/adnan99999977"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
               <span>Star</span>
             </Link>
-            <Link
-              target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
-            >
-              <CgGitFork />
-              <span>Fork</span>
-            </Link>
+           
           </div>
         </div>
       </div>
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,18 @@
 export const personalData = {
-  name: "ABU SAID",
-  profile: '/profile.png',
-  designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-}
+  name: "NAIF ADNAN",
+  profile: "/pic.png",
+  designation: "Full Stack Developer",
+  description:
+    "My name is NAIF ADNAN. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "naifadnan@gmail.com",
+  phone: "+8801839373767",
+  address: "57/13 romjan nagor,Manikganj, Dhaka, Bangladesh - 1212 ",
+  github: "https://github.com/adnan99999977",
+  facebook: "https://www.facebook.com/naif.adnan.90",
+  linkedIn: "https://www.linkedin.com/in/naif-adnan-1baa5a333/",
+  twitter: "https://x.com/NaifAdnan115902",
+  leetcode: "https://leetcode.com/u/Adnanboss001/",
+  devUsername:"Naif",
+  resume:
+    "https://drive.google.com/file/d/1zFnlQMqTD4EU-QITm-LqRPKd5lI3BAOy/view?usp=drive_link",
+};
